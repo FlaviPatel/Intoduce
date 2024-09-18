1a8cc8452dac1dd81f18b865ebb57ce66d82db96
Flavi Patel <divyapatel8166@gmail.com>
2024-09-18 18:51:16 +0530

    created an activty on introduction

---
+++ main.py
@@ -0,0 +1,8 @@
+# Define the function
+def introduce(n = "Riya"): # parameter
+    print("Hello I am", n)
+
+name = input("Enter your name:")
+# Call the function
+introduce(name) # argument
+introduce()
